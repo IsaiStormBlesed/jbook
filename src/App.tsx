200044c1f3269985a0b4f8258e76c5759d9000c8
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-08-30 23:37:01 +0000

    basic form elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+import { useState } from "react";
+
 export default function App() {
-  return <h1>JSBook</h1>;
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const handleClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      <pre></pre>
+    </div>
+  );
 }
