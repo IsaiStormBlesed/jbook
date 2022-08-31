9d937e5218a559bff4fce6026783639ab2a37cac
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-08-31 00:22:21 +0000

    transpiling works

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import { useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { useEffect, useRef, useState } from "react";
 
 export default function App() {
+  const ref = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const handleClick = () => {
-    console.log(input);
+  //startService initializes esbuild
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      wasmURL: "/esbuild.wasm",
+      worker: true,
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const handleClick = async () => {
+    if (!ref.current) return;
+
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    setCode(result.code);
   };
 
   return (
@@ -17,7 +38,7 @@ export default function App() {
       <div>
         <button onClick={handleClick}>Submit</button>
       </div>
-      <pre></pre>
+      <pre>{code}</pre>
     </div>
   );
 }
